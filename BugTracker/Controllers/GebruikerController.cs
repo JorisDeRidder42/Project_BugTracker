828ae8d85ef87f1292020360a26c09c1a5d0bd5d
Joris De Ridder <jorisderidder@outlook.be>
2021-12-22 19:34:15 +0100

    user viewmodel al gelinkt met list nu nog enkel de view aanmaken

--- BugTracker/Controllers/GebruikerController.cs
+++ BugTracker/Controllers/GebruikerController.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BugTracker.Models;
+using BugTracker.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BugTracker.Controllers
 {
     public class GebruikerController : Controller
     {
-        [Authorize]
+        private UserManager<User> _gebruikerManager;
+
+        public GebruikerController(UserManager<User> userManager)
+        {
+            _gebruikerManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            GebruikerListViewModel gviewModel = new GebruikerListViewModel()
+            {
+                Gebruikers = _gebruikerManager.Users.ToList()
+            };
+            return View(gviewModel);
         }
     }
 }
---
+++ BugTracker/ViewModel/GebruikerListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Models;
+using System.Collections.Generic;
+
+namespace BugTracker.ViewModel
+{
+    public class GebruikerListViewModel
+    {
+        public List<User> Gebruikers { get; set; }
+    }
+}
